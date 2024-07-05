b243421e06ef50036a41525f85240fc8f195033b
NoahDesktop <frag99@gmx.de>
2024-07-05 17:00:44 +0200

    menu enhancements

--- main.py
+++ main.py
@@ -1,39 +1,53 @@
 import file_handler
 
-careworkers = []
-residences = []
 
-while 1:
+def main():
+    careworkers = []
+    residences = []
 
-    print("\n********* Hauptmenü *********")
-    print("1: Pflegekräfte importieren")
-    print("2: Pflegekräfte exportieren")
-    print("3: Residenzen importieren")
-    print("4: Residenzen exportieren")
-    print("x: Beenden\n")
+    while 1:
 
-    choice = input("Option auswählen: ")
+        print("********* Hauptmenü *********")
+        print("1: Pflegekräfte importieren")
+        print("2: Pflegekräfte exportieren")
+        print("3: Residenzen importieren")
+        print("4: Residenzen exportieren")
+        print("x: Beenden\n")
 
-    match choice:
-        case "1":
-            path = input("Pfad angeben: ")
-            careworkers = file_handler.import_careworkers(json_file_path=path)
-            for cw in careworkers:
-                print(cw)
-        case "2":
-            path = input("Pfad angeben: ")
-            file_handler.export_careworkers(
-                json_file_path=path, careworkers=careworkers)
-        case "3":
-            path = input("Pfad angeben: ")
-            residences = file_handler.import_residences(json_file_path=path)
-            for res in residences:
-                print(res)
-        case "4":
-            path = input("Pfad angeben: ")
-            file_handler.export_residences(
-                json_file_path=path, residences=residences)
-        case "x":
-            exit()
-        case _:
-            print("Fehlerhafte Eingabe")
+        choice = input("Option auswählen: ")
+
+        match choice:
+            case "1":
+                path = input(
+                    "Pfad angeben (data/careworkers.json): ") or "data/careworkers.json"
+                careworkers = file_handler.import_careworkers(
+                    json_file_path=path)
+                for cw in careworkers:
+                    print(cw)
+            case "2":
+                path = input(
+                    "Pfad angeben (data/careworkers.json): ") or "data/careworkers.json"
+                file_handler.export_careworkers(
+                    json_file_path=path, careworkers=careworkers)
+            case "3":
+                path = input(
+                    "Pfad angeben (data/residences.json): ") or "data/residences.json"
+                residences = file_handler.import_residences(
+                    json_file_path=path)
+                for res in residences:
+                    print(res)
+            case "4":
+                path = input(
+                    "Pfad angeben (data/residences.json): ") or "data/residences.json"
+                file_handler.export_residences(
+                    json_file_path=path, residences=residences)
+            case "x":
+                exit()
+            case _:
+                print("Fehlerhafte Eingabe")
+
+        print("\n")
+
+
+if __name__ == "__main__":
+    main()
