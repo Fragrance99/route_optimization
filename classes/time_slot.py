f89fc1763c7378708bc42522dbfc61de8f07279d
NoahDesktop <frag99@gmx.de>
2024-07-05 11:53:31 +0200

    import of coworkers from json

--- classes/time_slot.py
+++ classes/time_slot.py
@@ -5,3 +5,6 @@ class TimeSlot:
     def __init__(self, beginning: time, end: time):
         self.beginning = beginning
         self.end = end
+
+    def __str__(self) -> str:
+        return f"Beginning: {self.beginning.isoformat()}, End: {self.end.isoformat()}"
--- importer.py
+++ importer.py
@@ -3,12 +3,37 @@ from classes.careworker import Careworker
 from classes.residence import Residence
 from classes.distance import Distance
 from classes.time_slot import TimeSlot
+from datetime import time
 
 
-def import_careworkers(json_file_path):
+def import_careworkers(json_file_path) -> list[Careworker]:
     with open(json_file_path, "r") as file:
         data = json.load(file)
 
     careworkers = []
     for cw_data in data['careworkers']:
-        pass
+        working_hours = []
+        for wh_data in cw_data['working_hours']:
+            beginning = time.fromisoformat(wh_data['beginning'])
+            end = time.fromisoformat(wh_data['end'])
+            time_slot = TimeSlot(beginning=beginning, end=end)
+            working_hours.append(time_slot)
+
+        careworker = Careworker(
+            unique_name=cw_data['unique_name'],
+            phone_number=cw_data['phone_number'],
+            level_of_care_competence=cw_data['level_of_care_competence'],
+            comment=cw_data['comment'],
+            maximum_working_hours=cw_data['maximum_working_hours'],
+            working_hours=working_hours
+        )
+        careworkers.append(careworker)
+
+    return careworkers
+
+
+if __name__ == "__main__":
+    careworkers = import_careworkers("data/careworkers.json")
+    for cw in careworkers:
+        print(f"Careworker: {cw.unique_name}, Max. working hours: {
+              cw.maximum_working_hours}, Working Hours: {'; '.join(map(TimeSlot.__str__, cw.working_hours))}")
